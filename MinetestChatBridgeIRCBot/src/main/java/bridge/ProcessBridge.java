100f315bb748706794fdd31140e37aa342e46dda
Lars Mueller <appgurulars@gmx.de>
2019-10-31 22:01:53 +0100

    🎃 Halloween Update

--- MinetestChatBridgeIRCBot/src/main/java/bridge/ProcessBridge.java
+++ MinetestChatBridgeIRCBot/src/main/java/bridge/ProcessBridge.java
@@ -1,1 +1,18 @@
-/home/lars/.minetest/mods/adv_chat/MinetestChatBridgeBot/src/main/java/bridge/ProcessBridge.java
+package bridge;
+
+import java.util.function.Consumer;
+
+public abstract class ProcessBridge {
+    public long last_ping;
+    public void ping() {
+        last_ping=System.currentTimeMillis();
+    }
+    
+    public abstract void kill(String reason);
+
+    public abstract void write(String out);
+
+    public abstract void serve();
+
+    public abstract void listen(Consumer<String> line_consumer);
+}
